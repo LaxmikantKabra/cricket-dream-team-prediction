000ec61d1890601472c1264ce5470ebff738c68e
laxmikantmkabra <143458925+laxmikantmkabra@users.noreply.github.com>
2023-09-17 21:51:42 -0400

    Delete ug directory

--- ug/Breast Cancer Risk Analysis/project execution files/backend/DjangoAPI/MyAPI/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-import MyAPI.views as views
-
-urlpatterns = [
-    path('prisk/', views.Risk_Amount.as_view(), name='risk_amount'),
-    path('sum/', views.sum, name='sum'),
-    path('add_values/', views.Add_Values.as_view(), name='api_add_values')
-]
-
